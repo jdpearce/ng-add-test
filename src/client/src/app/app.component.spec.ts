b606511271c08184b4b8bd485a70a07176b50e3a
Jo Hanna Pearce <jo@nrwl.io>
2019-09-16 15:03:50 +0100

    initial commit

--- src/client/src/app/app.component.spec.ts
+++ src/client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-add-test'`, () => {
+  it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-add-test');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-add-test app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
   });
 });
